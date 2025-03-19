a5c2f52ded9b59f9cf69f041b18225cadbcbe6cd
Sachin Sharma2 <thapak.krishna2125@gmail.com>
2025-03-20 00:14:33 +0530

    Added one more method to check anagram words .

--- src/Array_string/StringQuestions/FQanagram6.java
+++ src/Array_string/StringQuestions/FQanagram6.java
@@ -7,75 +7,42 @@ public class FQanagram6 {
     public static void main(String[] args) {
         String str1 = "little".toLowerCase();
         String str2 = "titlle".toLowerCase();
-        StringBuilder sb = new StringBuilder(26);
+    //Method 1--
+        char[] c1 = str1.toCharArray();
+        char[] c2 = str2.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
 
-        char []c1 = str1.toCharArray();
-        char []c2 = str2.toCharArray();
-       Arrays.sort(c1);
-       Arrays.sort(c2);
-
-        if(Arrays.toString(c1).equals(Arrays.toString(c2))){
-            System.out.println( "yess this is anagram");
-        }else{
+        if (Arrays.toString(c1).equals(Arrays.toString(c2))) {
+            System.out.println("yess this is anagram");
+        } else {
             System.out.println("nope not an anagram ");
         }
+    // Method 2--
+        int[] word1 = new int[26];
+        int[] word2 = new int[26];
+
+        for (int i = 0; i < str1.length(); i++) {
+            int ASCII1 = str1.charAt(i);
+            int index1 = ASCII1 - 97;
+            word1[index1]++;
+
+            int ASCII2 = str2.charAt(i);
+            int index2 = ASCII2 - 97;
+            word2[index2]++;
+        }
+        System.out.println(Arrays.toString(word1));
+        System.out.println(Arrays.toString(word2));
 
 
+        if (Arrays.toString(word1).equals(Arrays.toString(word2))) {
+            System.out.println("Yes it is an anagram");
 
+        } else {
+            System.out.println("Not an anagram");
+        }
     }
-}  //
- /*int[] ioi = new int[26];
-        int[] oio = new int[26];
-        if (str1.length() == str2.length()) {
-            for (int i = 0; i < str1.length(); i++) {
-                int ascii1 = str1.charAt(i);
-                int ascii2 = str2.charAt(i);
-                int index1 = ascii1 - 97;
-                int index2 = ascii2 - 97;
+}
 
-                ioi[index1] = str1.charAt(i);
-                oio[index2] = str2.charAt(i);
-
-            }
-            System.out.println(Arrays.toString(ioi));
-            System.out.println(Arrays.toString(oio));
-
-            int count = 0;
-            for (int i = 0; i < 26; i++) {
-                if (ioi[i] == oio[i])
-                    count++;
-            }
-            System.out.println(count);
-            if (count == 26) {
-                System.out.println("this is anagram");
-            } else {
-                System.out.println("not the anagram");
-            }
-        }*/
 
 //////////////////////////////////////////////////////////////////Another one
-
-/* int count=0;
-        if (str1.length() == str2.length()) {
-
-            for (int i = 0; i < str1.length(); i++) {
-
-                for (int j = 0; j < str2.length(); j++) {
-
-                        if (str1.charAt(i) == str2.charAt(j)) {
-                            count++;
-
-                        }
-                    }
-
-
-                }
-            } else {
-            System.out.println("length is not equal");
-        }
-        if (count == str1.length())
-
-            System.out.println(count + "Yes it is anagram");
-        else {
-            System.out.println(count + "Nope it is not anagram");
-        }*/
