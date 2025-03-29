c792ecd3637959d712d3005e036c68e6a48ba4b7
Sachin Sharma <codwork.tdotslash@gmail.com>
2025-03-30 03:43:44 +0530

    Two-pointer approach with while and for loops for shifting negatives
    The use of the `while` loop in the implementation is based on my previous understanding and approach to solving the problem.
    
    - Both implementations use the two-pointer technique for in-place rearrangement of negatives.
    - Retained both approaches: one with a while loop and the other with a for loop.
    - The choice of loop (while or for) doesn't matter; the important part is to use the approach that is more readable.So we can use either of the two approaches.
    - Ensured O(n) time complexity and O(1) space complexity for both methods.
    - Kept both approaches for reference and comparison.

---
+++ src/Array_string/ArrayQuestions/PQShift_All_Negatives_To_OneSide.java
@@ -0,0 +1,38 @@
+package Array_string.ArrayQuestions;
+
+import java.util.Arrays;
+
+public class PQShift_All_Negatives_To_OneSide {
+    public static void main(String[] args) {
+        int [] arr =  {1, 2, -3, -4, 5, -6};
+
+        int i = 0, m = 0;
+
+        while (m < arr.length) {
+            if(arr[m] < 0){
+                int temp = arr[i];
+                arr[i] = arr[m];
+                arr[m] = temp;
+                i++;
+                m++;
+            }else{
+                m++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+// Here both the loops work similarly just diff is the loop itself , one is while and other is for loop
+        // choice is totally upon us , by which we want to go
+        int [] arr1 =  {1, 2, -3, -4, 5, -6};
+
+        int j = 0;
+        for (int n = 0 ; n<arr1.length; n++){
+            if (arr1[n] < 0){
+                int temp = arr1[n];
+                arr1[n] = arr1[j];
+                arr1[j] = temp;
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(arr1));
+    }
+}
