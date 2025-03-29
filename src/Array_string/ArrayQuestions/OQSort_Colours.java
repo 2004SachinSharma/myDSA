ad166f38c5b39f0a87665ac110880600e5a466ab
Sachin Sharma <codwork.tdotslash@gmail.com>
2025-03-29 22:30:53 +0530

    Understood instructor's approach for sorting colors using the Three Pointer Algorithm (Dutch National Flag Problem).
    - This is the better approach then nested loop method with a more efficient algorithm to sort colors.
    - Utilized the instructor's approach, also known as the Dutch Flag Problem, with three pointers (`l`, `m`, `h`).
    - Optimized the algorithm to achieve O(n) time complexity by sorting `0`s, `1`s, and `2`s into separate sections of the array in a single pass.
    
    Summary of Changes:
    - first approach (O(n^2)) replaced with Three Pointer logic (O(n)).
    - Enhanced performance for larger input sizes by reducing unnecessary comparisons and swaps.

--- src/Array_string/ArrayQuestions/OQSort_Colours.java
+++ src/Array_string/ArrayQuestions/OQSort_Colours.java
@@ -3,23 +3,50 @@ package Array_string.ArrayQuestions;
 import java.util.Arrays;
 
 public class OQSort_Colours {
-   public static void main(String[] args) {
-       // source: Leet-Code, medium-level Question
-       //Here 0 represents red, 1 represents white and 2 represents blue
+    public static void main(String[] args) {
+        // source: Leet-Code, medium-level Question
+        //Here 0 represents red, 1 represents white and 2 represents blue
 
-       int [ ] arr = {1,0,2};
-       int val = 0;
+        int[] arr = {1, 0, 2};
+        int val = 0;
 
-       for(int i = 0; i<arr.length; i++){
-            for(int j = i; j<arr.length; j++){
-               if( arr[i] > arr[j] ){
-                   val = arr[i];
-                   arr[i] = arr[j] ;
-                   arr[j] = val;
-               }
-           }
-       }
-       System.out.println(Arrays.toString(arr));
-   }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    val = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = val;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+//--------------------------More efficient algo approach, (The Three pointer approach-----------
+
+        int[] arr2 = {1, 0, 2, 1, 0, 2};
+
+        int l = 0,m=0;
+        int h = arr2.length - 1;
+
+        while (m <= h) {
+            if (arr2[m] == 0) {
+                int temp = arr2[m];
+                arr2[m] = arr2[l];
+                arr2[l] = temp;
+                l++;
+                m++;
+
+            } else if (arr2[m] == 2) {
+                int temp = arr2[m];
+                arr2[m] = arr2[h];
+                arr2[h] = temp;
+                h--;
+
+            } else {
+                m++;
+            }
+        }
+        System.out.println(Arrays.toString(arr2));
+    }
 
 }
