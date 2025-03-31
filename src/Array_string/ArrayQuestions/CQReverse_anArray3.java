842eb6cc2531c9e30a4b48f2382e562b351978b2
Sachin Sharma <149469506+2004SachinSharma@users.noreply.github.com>
2025-04-01 02:09:18 +0530

    2ndRemove to unwanted text from the code

--- src/Array_string/ArrayQuestions/CQReverse_anArray3.java
+++ src/Array_string/ArrayQuestions/CQReverse_anArray3.java
@@ -6,8 +6,7 @@ public class CQReverse_anArray3 {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         int[] arr1 = {1, 2, 3, 4, 5};
-        int[] arr2 = {1, 2, 3, 4, 5};
-        int j = 0;
+        
 
         for (int i = arr.length-1; i >= 0; i--) {
 
