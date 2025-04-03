d2d6153e364be0bae471e86845d4578c764a45c1
Sachin Sharma <codwork.tdotslash@gmail.com>
2025-04-03 23:42:17 +0530

    feat: Implemented a simple sorting algorithm using nested loops
    - Used a straightforward approach with pairwise comparisons and swaps to sort the array.
    
    - Ensures in-place sorting without extra space.
    
    - Current time complexity: O(n²) due to nested iterations.
    
    - Space complexity: O(1) as sorting is done directly on the array.
    
    - Can be optimized by reducing unnecessary swaps, making it closer to Selection Sort (O(n²)).
    
    - Further improvements could lead to more efficient algorithms like Merge Sort or Quick Sort (O(n log n)).

--- src/Array_string/ArrayQuestions/GQSorting_an_Array3.java
+++ src/Array_string/ArrayQuestions/GQSorting_an_Array3.java
@@ -4,13 +4,24 @@ import java.util.Arrays;
 
 public class GQSorting_an_Array3 {
     public static void main(String[] args) {
-        int [] arr = {1,2,3,4,2,1,4,8,2,4,3};
-        Arrays.sort(arr);
+        int[] arr = {5, 3, 2, 1, 2, 0, 1, 2, 4, 0, 0, 1};
 
+        //This is Brute force algo ,it can be more efficient using other approaches like , Quick and merge sort!
+        //Time and space complexities are : O(n^2) and O(1) respectively.
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int j = i + 1;
+
+            while (j < arr.length) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                   j++;
+                }else j++;
+            }
+        }
         System.out.println(Arrays.toString(arr));
-        int i = 0;
-        int j = Byte.MAX_VALUE;
-        System.out.println(j);
 
     }
 }
