ad18dabb11c83cc3ca24b802900fe8268a1b192f
Sachin Sharma <codwork.tdotslash@gmail.com>
2025-04-29 15:16:40 +0530

    Refactor

--- src/Array_string/ArrayQuestions/RQCount_Pairs_with_Given_Sum.java
+++ src/Array_string/ArrayQuestions/RQCount_Pairs_with_Given_Sum.java
@@ -3,7 +3,7 @@ package Array_string.ArrayQuestions;
 public class RQCount_Pairs_with_Given_Sum {
     public static void main(String[] args) {
         int [] arr = {1,2,3,4,2,6,3,4};
-        int k = 5;
+        int k = 4;
         int c = 0;
         for ( int i = 0; i<arr.length; i++){
             for(int j = i+1; j<arr.length; j++ ){
@@ -14,9 +14,9 @@ public class RQCount_Pairs_with_Given_Sum {
             }
         }
         System.out.printf("Total pairs: %d \n",c );
-        // This Approach is Brute-force approach , it works correctly with time complexity of O(n log n)
+        // This Approach is the brute force approach, it works correctly with time complexity of O(n log n)
         // and space complexity of O(1) ,
-        // This approach can be more efficient,by using two pointer approach , and Hashmaps...
+        // This approach can be more efficient, by using two pointers approach, and Hashmaps...
 
             }
 }
